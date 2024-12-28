0956c449e6e24defd4ddfba86ae25dcc6ca0024a
MonicaTohe <Contacto.MonicaHf@gmail.com>
2024-12-28 03:25:55 -0300

    asas

--- src/components/MercadoPagoButton.js
+++ src/components/MercadoPagoButton.js
@@ -2,26 +2,24 @@ import React, { useEffect } from "react";
 
 const MercadoPagoButton = ({ preferenceId }) => {
   useEffect(() => {
-    // Cargar el SDK de Mercado Pago
+    if (!preferenceId) return; // No inicializar si no hay preferenceId
+
     const script = document.createElement("script");
     script.src = "https://sdk.mercadopago.com/js/v2";
     script.async = true;
     script.onload = () => {
-      // Inicializar Mercado Pago
       const mp = new window.MercadoPago(
-        "APP_USR-0b32ed69-db60-48b8-bc6c-4efbca600684",
+        "APP_USR-0b32ed69-db60-48b8-bc6c-4efbca600684", // Public Key
         { locale: "es-CL" }
       );
 
-      console.log("PASO", preferenceId);
-      // Renderizar el botón de pago
       mp.checkout({
         preference: {
-          id: preferenceId, // El ID de la preferencia generado por tu backend
+          id: preferenceId, // Usar el preferenceId recibido como prop
         },
         render: {
-          container: "#mercado-pago-button", // ID del contenedor donde se renderiza el botón
-          label: "Pagar con Mercado Pago", // Texto del botón
+          container: "#mercado-pago-button",
+          label: "Pagar con Mercado Pago",
         },
       });
     };
